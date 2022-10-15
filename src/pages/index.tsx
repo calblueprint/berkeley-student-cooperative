2b343cac6de16b37d8d829b7da3f8dd40678f75e
Gregoria  Millensifer <gregoriiaaa@berkeley.edu>
2022-10-14 17:26:29 -0700

    cleaned up the index file

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -20,105 +20,6 @@ import { getHouses } from "../firebase/queries/exampleQuery";
 import {addUser, deleteUser, updateUser, getUser, assignShiftToUser} from '../firebase/queries/user';
 
 const Home: NextPage = () => {
-  const [todos, setTodos] = useState<QueryDocumentSnapshot<DocumentData>[]>([]);
-  const [loading, setLoading] = useState<boolean>(true);
-
-  useEffect(() => {
-    getTodos();
-    setTimeout(() => {
-      setLoading(false);
-    }, 2000);
-  }, []);
-
-  const todosCollection = collection(firestore, "todos");
-
-  const getTodos = async () => {
-    const todosQuery = query(
-      todosCollection,
-      where("done", "==", false),
-      limit(10)
-    );
-    const querySnapshot = await getDocs(todosQuery);
-    const result: QueryDocumentSnapshot<DocumentData>[] = [];
-    querySnapshot.forEach((snapshot) => {
-      result.push(snapshot);
-    });
-    setTodos(result);
-  };
-
-  const updateTodo = async (documentId: string) => {
-    // create a pointer to the document id
-    const _todo = doc(firestore, `todos/${documentId}`);
-
-    // update the doc by setting done to true
-    await updateDoc(_todo, {
-      done: true,
-    });
-
-    // retrieve todos
-    getTodos();
-  };
-
-  const deleteTodo = async (documentId: string) => {
-    // create a pointer to the document id
-    const _todo = doc(firestore, `todos/${documentId}`);
-
-    // delete the doc
-    await deleteDoc(_todo);
-
-    // retrieve todos
-    getTodos();
-  };
-
-  const createUser = async () => {
-    addUser("bsc@berkeley.edu", "Euclid", "Sean", "Manager", firestoreAutoId());
-  }
-
-  const firestoreAutoId = (): string => {
-    const CHARS =
-      "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-    let autoId = "";
-    for (let i = 0; i < 20; i += 1) {
-      autoId += CHARS.charAt(Math.floor(Math.random() * CHARS.length));
-    }
-    return autoId;
-  };
-  
-
-
-  const retrieveUser = async () => {
-    let x = await getUser("mc8XQK7aiZW1dg8IC8v5");
-    if (x != null) {
-      console.log(x.availabilities);
-      console.log(x.shiftsAssigned);
-    }
-  }
-
-  const removeUser = async () => {
-    deleteUser("mc8XQK7aiZW1dg8IC8v5");//naming conflicts of subfct is same name as overall fct
-  }
-
-  const setUser = async () => {
-    let availabilities = new Map<string, Array<number>>();
-    let days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
-    for (let i = 0; i < days.length; i++) {
-      let newList = new Array<number>();
-      newList.push(0);
-      newList.push(2330);
-      availabilities.set(days[i], newList);
-    }
-    let newData = {
-      availabilities: mapToObject(availabilities)
-    }
-    updateUser("mc8XQK7aiZW1dg8IC8v5", newData);
-  }
-
-  const addShiftToUser = async () => {
-    await assignShiftToUser("mc8XQK7aiZW1dg8IC8v5", "1");
-    await retrieveUser();
-  }
-
-
   const mapToObject = (map: Map<any, any>): Object => {
     return Object.fromEntries(
       Array.from(map.entries(), ([k, v]) =>
@@ -137,56 +38,16 @@ const Home: NextPage = () => {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Todos app</title>
-        <meta name="description" content="Next.js firebase todos app" />
+        <title>Workshift App</title>
+        <meta name="description" content="Next.js firebase Workshift app" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <button onClick = {createUser}>Create </button>
-      <button onClick = {retrieveUser}>Get </button>
-      <button onClick = {removeUser}>Delete</button>
-      <button onClick = {setUser}>Set</button>
-      <button onClick = {addShiftToUser}>Assign Shift</button>
       <main className={styles.main}>
-        <h1 className={styles.title}>Todos app</h1>
-
-        <div className={styles.grid}>
-          {loading ? (
-            <div className={styles.card}>
-              <h2>Loading</h2>
-            </div>
-          ) : todos.length === 0 ? (
-            <div className={styles.card}>
-              <h2>No undone todos</h2>
-              <p>
-                Consider adding a todo from <Link href="/add-todo">here</Link>
-              </p>
-            </div>
-          ) : (
-            todos.map((todo, index) => {
-              return (
-                <div className={styles.card} key={index}>
-                  <h2>{todo.data().title}</h2>
-                  <p>{todo.data().description}</p>
-
-                  <div className={styles.cardActions}>
-                    <button type="button" onClick={() => updateTodo(todo.id)}>
-                      Mark as done
-                    </button>
-
-                    <button type="button" onClick={() => deleteTodo(todo.id)}>
-                      Delete
-                    </button>
-                  </div>
-                </div>
-              );
-            })
-          )}
-        </div>
+        <h1 className={styles.title}>Workshift App</h1>
       </main>
-
       <footer className={styles.footer}>
         <a href="#" rel="noopener noreferrer">
-          Todos app
+          Workshift App
         </a>
       </footer>
     </div>
