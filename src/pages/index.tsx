dcbafe2b362f3315ab46da610e113f1a78275fee
Gregoria  Millensifer <gregoriiaaa@berkeley.edu>
2022-10-14 17:31:46 -0700

    deleted unused imports in index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,40 +1,8 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import { firestore } from "../firebase/clientApp";
-import {
-  collection,
-  deleteDoc,
-  doc,
-  DocumentData,
-  getDocs,
-  limit,
-  query,
-  QueryDocumentSnapshot,
-  updateDoc,
-  where,
-} from "@firebase/firestore";
 import styles from "../styles/Home.module.css";
-import { useEffect, useState } from "react";
-import Link from "next/link";
-import { getHouses } from "../firebase/queries/exampleQuery";
-import {addUser, deleteUser, updateUser, getUser, assignShiftToUser} from '../firebase/queries/user';
 
 const Home: NextPage = () => {
-  const mapToObject = (map: Map<any, any>): Object => {
-    return Object.fromEntries(
-      Array.from(map.entries(), ([k, v]) =>
-        v instanceof Map ? [k, mapToObject(v)] : [k, v]
-      )
-    );
-  };
-
-  const objectToMap = (obj: Object): Map<any, any> => {
-    return new Map(
-        Array.from(Object.entries(obj), ([k, v]) =>
-        v instanceof Object ? [k, objectToMap(v)] : [k, v]
-        )
-    );
-  };
   return (
     <div className={styles.container}>
       <Head>
