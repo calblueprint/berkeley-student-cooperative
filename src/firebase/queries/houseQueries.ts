892698128d1d4ebcf5ef76455802dedb00b2172e
foloye <foloye11@gmail.com>
2022-10-18 18:52:21 -0700

    updated some functions

--- src/firebase/queries/houseQueries.ts
+++ src/firebase/queries/houseQueries.ts
@@ -1,9 +1,13 @@
-import { collection, addDoc, updateDoc, doc, getDoc, getDocs } from "firebase/firestore";
+import { collection, addDoc, updateDoc, doc, getDoc, getDocs, deleteDoc } from "firebase/firestore";
 import { firestore } from "../clientApp";
 import { House } from "../../types/schema";
+import { arrayBuffer } from "stream/consumers";
 
 
 const colRef = collection(firestore, "houses");
+
+//adding house will be done manually
+
 
 
 //grabs all houses from database
@@ -21,35 +25,22 @@ export const getAllHouses = async(): Promise<House[]>  => {
 
 
 //grabs a specific house from database
-export const getHouse = async(houseID: string): Promise<House[]>  => {
-    
+export const getHouse = async(houseID: string): Promise<House>  => {
     const docRef = doc(firestore, "houses", houseID);
-    const promises: Promise<House>[] = []; 
+
     const colSnap = await getDoc(docRef);
   
-    promises.push(parseHouse(colSnap));
+    const promise: Promise<House> = parseHouse(colSnap);
+    const house = await promise;
 
-    const houses = await Promise.all(promises);
-
-    return houses;
+    return house;
 
 }
 
 
-//updating the fields of house
-export const updateMembers = async (house: House, newMember: string): Promise<void> => {
-    const docRef = doc(firestore, "houses", house.houseID);
-
-    const addedMembers = house.members;
-    addedMembers?.push(newMember)
-    const data = {
-        members: addedMembers
-    }
-    await updateDoc(docRef, data)
-}
-
-export const updateAddress = async (house: House, newAddress: string): Promise<void> => {
-    const docRef = doc(firestore, "houses", house.houseID);
+//updating the fields of house, may not be useful ?
+export const updateAddress = async (houseID: string, newAddress: string): Promise<void> => {
+    const docRef = doc(firestore, "houses", houseID);
 
     const data = {
         address: newAddress
@@ -62,7 +53,7 @@ export const updateAddress = async (house: House, newAddress: string): Promise<v
 
 
 //parses house document passed in
-const parseHouse= async (doc : any) => {
+const parseHouse = async (doc : any) => {
     const data = doc.data();
     const houseID = doc.id;
     const members = doc.members;
@@ -71,3 +62,68 @@ const parseHouse= async (doc : any) => {
     const house = {houseID, members, address, schedule};
     return house as House;
 }
+
+
+//confirm how members array is going to work before trying to implement
+// how will we be parsing through the csv file ? is that how we are going to create the members list
+
+//main issue im running into is error handling for when the member array comes back as undefined,
+
+
+
+// export const addMember = async (houseID: string, newMember: string): Promise<void> => {
+//     const docRef = doc(firestore, "houses", houseID);
+//     // const promises: Promise<House>[] = []; 
+//     // const colSnap = await getDoc(docRef);
+//     // promises.push(parseHouse(colSnap));
+    
+//     const colSnap = await getDoc(docRef);
+    
+//     var addedMembers = (await parseHouse(colSnap)).members;
+    
+
+//     addedMembers?.push(newMember)
+//     const data = {
+//         members: addedMembers
+//     }
+//     await updateDoc(docRef, data)
+// }
+// export const deleteMember = async (houseID: string, oldMember: string) => {
+//     const docRef = doc(firestore, "houses", houseID);
+
+//     const promises: Promise<House>[] = []; 
+//     const colSnap = await getDoc(docRef);
+//     promises.push(parseHouse(colSnap));
+//     const houseMembers = (await promises[0]).members;
+//     const index = houseMembers?.indexOf(oldMember);
+//     if (index !== -1 && index){
+//         houseMembers?.splice(index, 1)
+//     }
+//     const data = {
+//         members: houseMembers
+//     }
+
+//     await updateDoc(docRef, data);
+// }
+
+
+//applies to comments above, but goes in index.tsx
+  // const addMemberFB = async (houseID :string, newMember:string) =>{
+  //   var addAMember = await addMember(houseID, newMember);
+  //   console.log("added member", newMember);
+  // }
+  // const removeMemberFB = async (houseID :string, oldMember:string) =>{
+  //   var rmAMember = await deleteMember(houseID, oldMember);
+  //   console.log("removed member", rmAMember);
+  // }
+
+    //gets all houses from firebase
+    // const getAllHouseFB = async () =>{
+    //     var fireHouse = await getAllHouses();
+    //     setHouses(fireHouse);
+    //   }
+    //   //gets specific house from firebase, must specify certain house
+    //   const getHouseFB = async (houseID :string) =>{
+    //     var fireAHouse = await getHouse(houseID);
+    //     setCurrHouse(fireAHouse);
+    //   }
