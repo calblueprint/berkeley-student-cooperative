ae0e93797049b0bf8043d1c30cf2e0e0cf29046f
Steven Huang <stevenhuang010@gmail.com>
2022-10-22 14:33:41 -0700

    support unassign

--- src/pages/shiftAssignmentTable.tsx
+++ src/pages/shiftAssignmentTable.tsx
@@ -6,7 +6,6 @@ import TableCell from "@mui/material/TableCell";
 import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
-import { useState } from "react";
 import { User } from "../types/schema";
 
 
