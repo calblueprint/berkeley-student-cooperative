5fa562b4a8180ee3f123d9baaf5cc9beab9781e1
Danashi Messele <danashimessele@berkeley.edu>
2022-10-28 18:40:56 -0700

    update imports

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import styles from "./Layout.module.css";
-import MemberNavbar from "../MemberComponents/Navbar/Navbar";
+import MemberNavbar from "../MemberComponents/Navbar/MemberNavbar";
 import Head from "next/head";
 
 const Layout = ({ children, title }: any) => {
