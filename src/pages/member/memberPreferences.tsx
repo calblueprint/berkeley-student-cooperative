150d45a4383e45b19f2b0c17028417715a391839
foloye <foloye11@gmail.com>
2022-10-28 17:04:56 -0700

    added some house queries

--- src/pages/member/memberPreferences.tsx
+++ src/pages/member/memberPreferences.tsx
@@ -5,17 +5,34 @@ import {Typography, Table, TableBody, TableCell, TableContainer, TableHead, Tabl
 import styles from './memberPreferences.module.css'
 
 const PreferButton = () => {
+    //logic needs to be added, but something will be done when a certain button is pressed
+    const handlePrefButton = (prefValue : String) => {
+        if (prefValue == 'dislike'){
+            console.log("dislike pushed")
+        } else if (prefValue == 'neutral') {
+            console.log("neutral pushed")
+        } else if (prefValue == 'prefer') {
+            console.log("prefer pushed")
+        }
+    }
+    
     return (
+        //button group that puts all buttons in one component, just to make code less redundant
         <ButtonGroup variant="outlined" aria-label="outlined button group">
-            <Button>Dislike</Button>
-            <Button>Neutral</Button>
-            <Button>Prefer</Button>
+            <Button onClick={() => handlePrefButton('dislike')}>Dislike</Button>
+            <Button onClick={() => handlePrefButton('neutral')}>Neutral</Button>
+            <Button onClick={() => handlePrefButton('prefer')}>Prefer</Button>
         </ButtonGroup>
     );
 };
 
 const MemberPreferences = () => {
+    //array that holds the categories of particular house
     const [houseCategories, setHouseCategories] = useState<String[]>()
+    const [preferID, setPreferID] = useState<String>();
+    
+    //gets all categories from the backend
+    //Euclid is hardcoded in so info appears.
     useEffect(() => {
         getCategories('EUC').then((value) => {
             
@@ -24,14 +41,19 @@ const MemberPreferences = () => {
         });
     }, []);
 
+    
+
+    //change when shift queries allow to get by categories
     const dummyArr = ["cook breakie", "cook din din", "prepare lunch"]
     return (
         <div className={styles.memberPrefPage}>
+            {/* title of page */}
             <div className={styles.title}>
                 <Typography className={styles.prevPage} variant='h2'>Settings/</Typography> 
                 <Typography className={styles.taskTitle} variant='h2'>Task Preferences</Typography>    
             </div>
             <div className={styles.prefTables}>
+                {/* maps through house categories and creates a table for each table */}
                 {houseCategories?.map((item,index) => {
                     return (
                         <div key={index} className={styles.spefTable} >
@@ -41,18 +63,21 @@ const MemberPreferences = () => {
                                         <TableRow>
                                             <TableCell>{item}</TableCell>
                                             <TableCell align="right">
-                                                <PreferButton/>
+                                                <PreferButton />
                                             </TableCell>
                                         </TableRow>
                                         
                                     </TableHead>
+
+                                    {/* goes through each task and creates a row with a button that determines user's preference */}
+                                    {/* should be changed when able to grab tasks for each category */}
                                     <TableBody>
                                         {dummyArr.map((shift, idx) => {
                                             return (
                                                 <TableRow key={idx}>
                                                     <TableCell>{shift}</TableCell>
                                                     <TableCell align="right">
-                                                        <PreferButton/>
+                                                        <PreferButton />
                                                     </TableCell>
                                                 </TableRow>
                                             )
@@ -64,11 +89,7 @@ const MemberPreferences = () => {
                     )
                 })}
                 
-                <TableContainer>
-                            <Table>
-
-                            </Table>
-                        </TableContainer>
+               
             </div>
         
         </div>
