ca9da2b766aaf5507bfb74a49fbff134fa37782d
Gregoria  Millensifer <gregoriiaaa@berkeley.edu>
2022-11-02 04:16:41 -0700

    adding create shift and assign shift components for demo

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,7 @@
 import type { NextPage } from "next";
 import Head from "next/head";
+import AssignShiftcard from "../components/ManagerComponents/AssignShiftcard/AssignShiftcard";
+import ShiftCard from "../components/ManagerComponents/Shiftcard/Shiftcard";
 import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
@@ -10,6 +12,8 @@ const Home: NextPage = () => {
         <meta name="description" content="Next.js firebase Workshift app" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <ShiftCard />
+      <AssignShiftcard shiftID={"KGA1GPrcoFUqjVc6bUSh"} houseID={"EUC"} />
       <footer className={styles.footer}>
         <a href="#" rel="noopener noreferrer">
           Workshift App
