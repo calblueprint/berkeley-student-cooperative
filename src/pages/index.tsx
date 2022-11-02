48c70921c8e4eb2caed5916309e5b07ddeb37263
9samioh <63632947+9samioh@users.noreply.github.com>
2022-11-02 09:33:41 -0700

    Sami/member settings availability (#13)
    * blind coding
    
    * update shift queries
    
    * fin shift setup
    
    * updated
    
    * finished creating the assign shift card
    
    * fixed variable switch and made managerComponents folder
    
    * changed to 12 hour date
    
    * user settings info card
    
    * availability info for the member settings page
    
    * commenting out the 'Last updated...' part for now
    
    Co-authored-by: Steven Huang <stevenhuang010@gmail.com>
    Co-authored-by: Gregoria  Millensifer <gregoriiaaa@berkeley.edu>
    Co-authored-by: gregoriiaaa <66578584+gregoriiaaa@users.noreply.github.com>

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,15 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
+import { useEffect, useState } from "react";
+import {
+  getAllHouses,
+  getHouse,
+  updateAddress,
+} from "../firebase/queries/house";
+import { House } from "../types/schema";
+import SettingsInfo from "../components/MemberComponents/SettingsInfo/SettingsInfo";
+import AvailabilityInfo from "../components/MemberComponents/AvailabilityInfo/AvailabilityInfo";
 
 const Home: NextPage = () => {
   return (
@@ -10,6 +19,13 @@ const Home: NextPage = () => {
         <meta name="description" content="Next.js firebase Workshift app" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <main className={styles.main}>
+        <h1 className={styles.title}>Workshift App</h1>
+        <ShiftCard />
+        <AssignShiftcard shiftID={"iBVA4gOntEGFA4AxpqFU"} houseID={"EUC"} />
+        <SettingsInfo userID={"1234"}/>
+        <AvailabilityInfo userID={"1234"}/>
+      </main>
       <footer className={styles.footer}>
         <a href="#" rel="noopener noreferrer">
           Workshift App
