9226f89a95a6c7eec875138c2247bc528c3457e4
danashimessele <90217967+danashimessele@users.noreply.github.com>
2022-11-04 16:53:13 -0700

    Danashi/user context (#8)
    * fin user queries
    
    * update user availabiltiies
    
    * fin user queries
    
    * added add shift to user helper
    
    * communicates to firebase auth correctly
    
    * insig
    
    * Buttons support queries, register/sign in can retrieve users from FB
    
    * renamed to authUser
    
    * added establishUserContext to return and be used
    
    * added deleteUser to auth
    
    * pulled from main and fixed merge conflicts
    
    * added house object to user context, separated context to different file
    
    * missing imports
    
    Co-authored-by: Steven Huang <stevenhuang010@gmail.com>
    Co-authored-by: Andrei Tan <ManTan.875@berkeley.edu>
    Co-authored-by: gregoriiaaa <66578584+gregoriiaaa@users.noreply.github.com>

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,11 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useContext } from "react";
+import Link from "next/link";
+import {addUser, deleteUser, updateUser, getUser, assignShiftToUser} from '../firebase/queries/user';
+import { User } from "../types/schema";
+import { useUserContext } from "../context/UserContext";
 import {
   getAllHouses,
   getHouse,
@@ -10,8 +14,76 @@ import {
 import { House } from "../types/schema";
 import SettingsInfo from "../components/MemberComponents/SettingsInfo/SettingsInfo";
 import AvailabilityInfo from "../components/MemberComponents/AvailabilityInfo/AvailabilityInfo";
+import AssignShiftcard from "../components/ManagerComponents/AssignShiftcard/AssignShiftcard";
+import ShiftCard from "../components/ManagerComponents/Shiftcard/Shiftcard";
 
 const Home: NextPage = () => {
+
+	//getting all possible fields from auth; authUSER is current signed in user, house is the house object
+	const { authUser, house, register, signIn, signOutAuth, establishUserContext } = useUserContext();
+
+  const createUser = async () => {
+    addUser("bsc@berkeley.edu", "Euclid", "Sean", "Manager", firestoreAutoId());
+  }
+
+  const firestoreAutoId = (): string => {
+    const CHARS =
+      "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    let autoId = "";
+    for (let i = 0; i < 20; i += 1) {
+      autoId += CHARS.charAt(Math.floor(Math.random() * CHARS.length));
+    }
+    return autoId;
+  };
+
+  const retrieveUser = async () => {
+    let x = await getUser("mc8XQK7aiZW1dg8IC8v5");
+    if (x != null) {
+      console.log(x.availabilities);
+      console.log(x.shiftsAssigned);
+    }
+  }
+
+  const removeUser = async () => {
+    deleteUser("SIH5XjDtdtNhiGb91Sq976Pl0Zc2");//naming conflicts of subfct is same name as overall fct
+  }
+  
+  const setUser = async () => {
+    let availabilities = new Map<string, Array<number>>();
+    let days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+    for (let i = 0; i < days.length; i++) {
+      let newList = new Array<number>();
+      newList.push(0);
+      newList.push(2330);
+      availabilities.set(days[i], newList);
+    }
+    let newData = {
+      availabilities: mapToObject(availabilities)
+    }
+    updateUser("mc8XQK7aiZW1dg8IC8v5", newData);
+  }
+
+  const addShiftToUser = async () => {
+    await assignShiftToUser("mc8XQK7aiZW1dg8IC8v5", "1");
+    await retrieveUser();
+  }
+
+  const mapToObject = (map: Map<any, any>): Object => {
+    return Object.fromEntries(
+      Array.from(map.entries(), ([k, v]) =>
+        v instanceof Map ? [k, mapToObject(v)] : [k, v]
+      )
+    );
+  };
+
+  const objectToMap = (obj: Object): Map<any, any> => {
+    return new Map(
+        Array.from(Object.entries(obj), ([k, v]) =>
+        v instanceof Object ? [k, objectToMap(v)] : [k, v]
+        )
+    );
+  };
+
   return (
     <div className={styles.container}>
       <Head>
@@ -36,3 +108,4 @@ const Home: NextPage = () => {
 };
 
 export default Home;
+
