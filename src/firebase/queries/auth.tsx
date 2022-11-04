9533b34b602731466b6e2778a9d27c12b0ae6367
danashimessele <90217967+danashimessele@users.noreply.github.com>
2022-11-04 16:53:13 -0700

    Danashi/user context (#8)
    * fin user queries
    
    * update user availabiltiies
    
    * fin user queries
    
    * added add shift to user helper
    
    * communicates to firebase auth correctly
    
    * insig
    
    * Buttons support queries, register/sign in can retrieve users from FB
    
    * renamed to authUser
    
    * added establishUserContext to return and be used
    
    * added deleteUser to auth
    
    * pulled from main and fixed merge conflicts
    
    * added house object to user context, separated context to different file
    
    * missing imports
    
    Co-authored-by: Steven Huang <stevenhuang010@gmail.com>
    Co-authored-by: Andrei Tan <ManTan.875@berkeley.edu>
    Co-authored-by: gregoriiaaa <66578584+gregoriiaaa@users.noreply.github.com>

---
+++ src/firebase/queries/auth.tsx
@@ -0,0 +1,115 @@
+import { getAuth, createUserWithEmailAndPassword, getAdditionalUserInfo, signInWithEmailAndPassword, signOut, onAuthStateChanged} from "firebase/auth";
+import { addUser, getUser } from "./user";
+import React, { createContext, useContext, useState } from "react";
+import { User} from "../../types/schema";
+import { defaultUser } from "./user";
+import { defaultHouse } from "./house";
+import { doc, deleteDoc } from "firebase/firestore";
+import { firestore } from "../clientApp";
+import { getHouse } from "./house";
+
+//managers don't have to register.  House will be matched to email
+//userID is going to get generated in register.
+/*
+    1. Have them register
+    2. Check the email used in register, match it to an email in a csv
+    3. Retrieve the House from this CSV
+    4. If this works, continue and generate a userID
+    5. Pass all of this info to addUser.
+*/
+
+export const useFirebaseAuth = () => {
+	const auth = getAuth();
+	const [authUser, setAuthUser] = useState(defaultUser);
+	const [house, setHouse] = useState(defaultHouse)
+
+	const register = async (
+			email: string,
+			name: string,
+			password: string
+	): Promise<void> => {
+			try {
+					//PENDING: Search for email in CSV once this func is available.
+					createUserWithEmailAndPassword(auth, email, password )
+					.then((userCredential) => {
+						const user = userCredential.user;
+						console.log("Created User:", user);
+						/* 
+							Signed in
+							PENDING Completion of addUser
+							PENDING Role is automatically resident/ not Manager
+							PENDING HouseID found in csv
+							addUser(email, houseID, name, role, user.uid)
+						*/
+					addUser(email, "Euclid", name, "Member", user.uid).then(() => {
+						establishUserContext(user.uid);
+					});
+					})
+			} catch(e) {
+				console.error(e);
+				throw e
+			}
+	};
+
+	const signIn = async (
+		email: string,
+		password: string
+	) => {
+		try {
+			console.log("Email: ", email, " Password: ", password);
+			signInWithEmailAndPassword(auth, email, password)
+			.then((userCredential) => {
+				const userID= userCredential.user.uid;
+				establishUserContext(userID);
+			})
+		} catch(e) {
+			console.error(e);
+			throw e;
+		}
+	};
+
+	const signOutAuth = async (): Promise<void> => {
+		try {
+			await signOut(auth);
+			setAuthUser(defaultUser);
+			console.log("Signed Out!!");
+		} catch (e) {
+			console.error(e);
+			throw e;
+		}
+	};
+
+	
+	const establishUserContext = async(uid: string): Promise<void> => {
+		try {
+			getUser(uid).then((userFromDoc) => {
+				if (userFromDoc != null) {
+					console.log("USER FROM FIREBASE: ", userFromDoc);
+					setAuthUser(userFromDoc);
+					getHouse(userFromDoc.houseID).then((houseFromDoc) => {
+						console.log("HOUSE FROM FIREBASE:", houseFromDoc)
+						setHouse(houseFromDoc)
+					})
+				} else {
+					console.log("user does not exist");
+				}
+			})
+		} catch (e) {
+		}
+	}
+
+
+	const deleteUser = async(uid: string): Promise<void> => {
+		await deleteDoc(doc(firestore, "users", uid ));
+	}
+	
+	return {
+		authUser,
+		house, 
+		register, 
+		signIn, 
+		signOutAuth,
+		establishUserContext,
+		deleteUser, 
+	};
+}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,22 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+<<<<<<< HEAD
+<<<<<<< HEAD
+import { AuthUserProvider } from '../context/UserContext'
+
+=======
  
+>>>>>>> 48c70921c8e4eb2caed5916309e5b07ddeb37263
+=======
+import { AuthUserProvider } from '../context/UserContext'
+
+>>>>>>> fa275c2d4f60066309321f637d0c57eb791cbcc6
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+	return (
+	  <AuthUserProvider>
+			<Component {...pageProps} />
+	  </AuthUserProvider>
+	)
 }
 
 export default MyApp
