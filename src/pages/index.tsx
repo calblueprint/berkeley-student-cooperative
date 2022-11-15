b2f8b4683c31b318e1c16a1f3f73973fa6191cef
Gregoria  Millensifer <gregoriiaaa@berkeley.edu>
2022-11-15 14:24:00 -0800

    cleaned up index file

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,6 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
-import { useEffect, useState, useContext } from "react";
-import Link from "next/link";
-import {addUser, deleteUser, updateUser, getUser, assignShiftToUser} from '../firebase/queries/user';
-import { User } from "../types/schema";
-import { useUserContext } from "../context/UserContext";
-import {
-  getAllHouses,
-  getHouse,
-  updateAddress,
-} from "../firebase/queries/house";
-import { House } from "../types/schema";
 import Layout from "../components/Layout/Layout";
 import SettingsInfo from "../components/MemberComponents/SettingsInfo/SettingsInfo";
 import AvailabilityInfo from "../components/MemberComponents/AvailabilityInfo/AvailabilityInfo";
@@ -20,25 +9,25 @@ import ShiftCard from "../components/ManagerComponents/Shiftcard/Shiftcard";
 
 const Home: NextPage = () => {
   return (
-		<Layout>
-			<div className={styles.container}>
-				<Head>
-					<title>Workshift App</title>
-					<meta name="description" content="Next.js firebase Workshift app" />
-					<link rel="icon" href="/favicon.ico" />
-				</Head>
-				<main className={styles.main}>
-					<h1 className={styles.title}>Workshift App</h1>
-					<ShiftCard />
-					<AssignShiftcard shiftID={"KGA1GPrcoFUqjVc6bUSh"} houseID={"EUC"} />
-				</main>
-				<footer className={styles.footer}>
-					<a href="#" rel="noopener noreferrer">
-						Workshift App
-					</a>
-				</footer>
-			</div>
-		</Layout>
+    <Layout>
+      <div className={styles.container}>
+        <Head>
+          <title>Workshift App</title>
+          <meta name="description" content="Next.js firebase Workshift app" />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <main className={styles.main}>
+          <h1 className={styles.title}>Workshift App</h1>
+          <ShiftCard />
+          <AssignShiftcard shiftID={"KGA1GPrcoFUqjVc6bUSh"} houseID={"EUC"} />
+        </main>
+        <footer className={styles.footer}>
+          <a href="#" rel="noopener noreferrer">
+            Workshift App
+          </a>
+        </footer>
+      </div>
+    </Layout>
   );
 };
 
