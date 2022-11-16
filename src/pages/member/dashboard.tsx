8d4a6754f26f5355deb22d3c4d23ec1e1e336cc8
Steven Huang <stevenhuang010@gmail.com>
2022-11-15 19:48:29 -0800

    only need to do submnit

--- src/pages/member/dashboard.tsx
+++ src/pages/member/dashboard.tsx
@@ -2,7 +2,7 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import styles from "../../styles/Home.module.css";
 import Layout from "../../components/Layout/Layout";
-
+import CreateAccountPage from "../createAccountPage";
 const Home: NextPage = () => {
   return (
     <Layout>
@@ -10,8 +10,10 @@ const Home: NextPage = () => {
         <Head>
           <title>Members Dashboard</title>
         </Head>
+        
         <main className={styles.main}>
           <h1 className={styles.title}>Members Dashboard</h1>
+          <CreateAccountPage></CreateAccountPage>
         </main>
         <footer className={styles.footer}>
           <a href="#" rel="noopener noreferrer">
