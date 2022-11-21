57fdace51cb180720e0076f5396e97cda097cf5f
Steven Huang <stevenhuang010@gmail.com>
2022-11-20 17:02:08 -0800

    need to do submit

--- src/pages/member/dashboard.tsx
+++ src/pages/member/dashboard.tsx
@@ -2,6 +2,8 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import styles from "../../styles/Home.module.css";
 import Layout from "../../components/Layout/Layout";
+import AvailabilityEntry from "../../components/MemberComponents/AvailabilityEntry/AvailabilityEntry";
+import AvailabilityDaySelection from "../../components/MemberComponents/AvailabilityEntry/AvailabilityDaySelection";
 
 const Home: NextPage = () => {
   return (
@@ -10,10 +12,10 @@ const Home: NextPage = () => {
         <Head>
           <title>Members Dashboard</title>
         </Head>
-        
         <main className={styles.main}>
           <h1 className={styles.title}>Members Dashboard</h1>
         </main>
+        <AvailabilityDaySelection userID = "1"></AvailabilityDaySelection>
         <footer className={styles.footer}>
           <a href="#" rel="noopener noreferrer">
             Workshift App
