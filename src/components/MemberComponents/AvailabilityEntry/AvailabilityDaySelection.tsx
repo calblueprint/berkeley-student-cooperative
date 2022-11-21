1c5eac78a613ead8a34198815bc84e281ba00cd7
Steven Huang <stevenhuang010@gmail.com>
2022-11-20 17:41:44 -0800

    fin

--- src/components/MemberComponents/AvailabilityEntry/AvailabilityDaySelection.tsx
+++ src/components/MemberComponents/AvailabilityEntry/AvailabilityDaySelection.tsx
@@ -1,26 +1,37 @@
 import { useState } from "react";
 import AvailabilityEntry from "./AvailabilityEntry";
-import { days } from '../../../firebase/helpers';
+import { mergeMap, days, mapToObject } from '../../../firebase/helpers';
 import { Button } from "@mui/material";
 import styles from './AvailabilityEntry.module.css';
+import { updateUser } from "../../../firebase/queries/user";
 
 type AvailabilityDaySelectionProps = {
     userID: string
 }
 
 const AvailabilityDaySelect: React.FC<AvailabilityDaySelectionProps> = ({userID}: AvailabilityDaySelectionProps) => {
+
+    const initializeMap = () => {
+        let m = new Map<string, number[]>();
+        for (let i = 0; i < days.length; i++) {
+            m.set(days[i], []);
+        }
+        return m;
+    }
+
     const [selectedDay, setSelectedDay] = useState<string>("Monday");
+    const [assignedMap, setAssignedMap] = useState<Map<string, number[]>>(initializeMap());
 
     const handleClick = (day: string) => {
         setSelectedDay(day);
     }
 
-    const handleSubmit = () => {
-        // get all availability boxes for a day in availability entry
-        for (let i = 0; i < days.length; i++) {
-            let day = days[i];
-            
+    const handleSubmit = async () => {
+        let newMap = mergeMap(assignedMap);
+        let newData = {
+            availabilities: mapToObject(newMap)
         }
+        // await updateUser(userID, newData);
     }
 
     return (
@@ -32,7 +43,7 @@ const AvailabilityDaySelect: React.FC<AvailabilityDaySelectionProps> = ({userID}
             </div>
             {
                 days.map((day) => (
-                    <AvailabilityEntry userID = {userID} day = {day} selectedDay = {selectedDay}/>
+                    <AvailabilityEntry key = {day} userID = {userID} day = {day} selectedDay = {selectedDay} assignedMap = {assignedMap}/>
                 ))
             }
             <Button onClick = {handleSubmit}>Submit</Button>
