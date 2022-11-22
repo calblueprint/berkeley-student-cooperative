4de785944ae86b71d61d5ca24512cd5d1759478e
Gregoria  Millensifer <gregoriiaaa@berkeley.edu>
2022-11-22 15:03:53 -0800

    fixed merge error

--- src/firebase/helpers.ts
+++ src/firebase/helpers.ts
@@ -120,3 +120,5 @@ export const mergeMap = (map: Map<string, number[]>) => {
 export const mapToJSON = (map: Map<any, any>): string => {
   return JSON.stringify(mapToObject(map));
 }
+
+export const emailRegex = new RegExp(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
