a62892eb4fdd74a8fc9ff882f86b335152e6e333
Steven Huang <63945057+stevenhuang010@users.noreply.github.com>
2022-11-22 17:59:55 -0800

    push main (#31)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -38,7 +38,7 @@ const Home: NextPage = () => {
           <button onClick={() => register("testing_register@gmail.com", "EUC", "Greg", "M", "Member", "testing123")}>Register</button>
 					<button onClick={() => signIn("eucwm@bsc.coop", "euclidmanager")}>Sign In</button>
 					<button onClick={() => signOutAuth()}>Sign Out</button>
-					<h1>{authUser ? authUser.first_name + " " + authUser.last_name : "not signed in"}</h1>
+					<h1>{authUser ? authUser.firstName + " " + authUser.lastName : "not signed in"}</h1>
         </main>
         <footer className={styles.footer}>
           <a href="#" rel="noopener noreferrer">
