59a144011a87a3321dde7bc5ba39e6685aea37d2
Steven Huang <stevenhuang010@gmail.com>
2022-11-22 18:33:17 -0800

    fix

--- src/pages/member/dashboard/index.tsx
+++ src/pages/member/dashboard/index.tsx
@@ -3,8 +3,11 @@ import Head from "next/head";
 import styles from "./Dashboard.module.css";
 import Layout from "../../../components/Layout/Layout";
 import AvailabilityInputModal from "../../../components/MemberComponents/AvailabilityEntry/AvailabilityInputModal";
+import { useFirebaseAuth } from "../../../firebase/queries/auth";
 
 const Home: NextPage = () => {
+  const { authUser } = useFirebaseAuth();
+  
   return (
     <Layout>
       <div className={styles.container}>
@@ -14,7 +17,7 @@ const Home: NextPage = () => {
         <main className={styles.main}>
           <h1 className={styles.title}>Members Dashboard</h1>
         </main>
-        <AvailabilityInputModal userID="1"></AvailabilityInputModal>
+        <AvailabilityInputModal userID = {authUser.userID}></AvailabilityInputModal>
         <footer className={styles.footer}>
           <a href="#" rel="noopener noreferrer">
             Workshift App
