11da84e06c1fa83f7e5758cd447e95ae53ef5d3d
9samioh <63632947+9samioh@users.noreply.github.com>
2022-11-22 18:18:57 -0800

    finished login page and nav bars (#30)
    * finished login page and nav bars
    
    * added link to create account page
    
    * fixed Member and member error

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,54 +1,19 @@
 import type { NextPage } from "next";
-import Head from "next/head";
-import styles from "../styles/Home.module.css";
-import Layout from "../components/Layout/Layout";
-import SettingsInfo from "../components/MemberComponents/SettingsInfo/SettingsInfo";
-import AvailabilityInfo from "../components/MemberComponents/AvailabilityInfo/AvailabilityInfo";
-import AssignShiftcard from "../components/ManagerComponents/AssignShiftcard/AssignShiftcard";
-import ShiftCard from "../components/ManagerComponents/Shiftcard/Shiftcard";
-import router from "next/router";
-import { addUser } from "../firebase/queries/user";
-import { useUserContext } from "../context/UserContext";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+import { useFirebaseAuth } from "../firebase/queries/auth";
 
 const Home: NextPage = () => {
+  const router = useRouter();
+  const { authUser } = useFirebaseAuth();
 
-	const { authUser, signIn, register, signOutAuth} = useUserContext()
+  useEffect(() => {
+    if (authUser.userID == "") {
+      router.push("/login");
+    }
+  });
 
-  return (
-    <Layout>
-      <div className={styles.container}>
-        <Head>
-          <title>Workshift App</title>
-          <meta name="description" content="Next.js firebase Workshift app" />
-          <link rel="icon" href="/favicon.ico" />
-        </Head>
-        <main className={styles.main}>
-          <h1 className={styles.title}>Workshift App</h1>
-          <ShiftCard />
-          <AssignShiftcard shiftID={"KGA1GPrcoFUqjVc6bUSh"} houseID={"EUC"} />
-          <button
-            onClick={() => {
-              router.push("/ParseCsv/ParseCsv");
-            }}
-          >
-            Parse
-          </button>
-          <button onClick={() => register("testing_register@gmail.com", "EUC", "Greg", "M", "Member", "testing123")}>Register</button>
-					<button onClick={() => signIn("eucwm@bsc.coop", "euclidmanager")}>Sign In</button>
-					<button onClick={() => signOutAuth()}>Sign Out</button>
-					<h1>{authUser ? authUser.firstName + " " + authUser.lastName : "not signed in"}</h1>
-        </main>
-        <footer className={styles.footer}>
-          <a href="#" rel="noopener noreferrer">
-            Workshift App
-          </a>
-        </footer>
-      </div>
-    </Layout>
-  );
+  return (<div/>)
 };
 
 export default Home;
-function firestoreAutoId(): string {
-  throw new Error("Function not implemented.");
-}
