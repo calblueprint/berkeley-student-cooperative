8eae3e1a4893a3e007eb814b635f94d09c0d3dcc
ken-mar <kenmartinez@berkeley.edu>
2022-11-22 17:51:43 -0800

    fixed merge conflict in index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -52,6 +52,6 @@ const Home: NextPage = () => {
 };
 
 export default Home;
-function firestoreAutoId(): string {
-  throw new Error("Function not implemented.");
-}
+// function firestoreAutoId(): string {
+//   throw new Error("Function not implemented.");
+// }
