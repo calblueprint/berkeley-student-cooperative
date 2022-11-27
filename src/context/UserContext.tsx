70097ab8eacdaa13f8595750fa098bd4552d2eda
Gregoria  Millensifer <gregoriiaaa@berkeley.edu>
2022-11-26 19:01:37 -0800

    trying to get user context to persist

--- src/context/UserContext.tsx
+++ src/context/UserContext.tsx
@@ -1,29 +1,41 @@
+import React, { useState } from "react";
 import { createContext, useContext } from "react";
 import { useFirebaseAuth } from "../firebase/queries/auth";
 import { defaultUser } from "../firebase/queries/user";
 import { defaultHouse } from "../firebase/queries/house";
+import { User } from "firebase/auth";
+import { userAgent } from "next/server";
 
 const authUserContext = createContext({
-  authUser: defaultUser,
-  house: defaultHouse,
-  register: async (
-    email: string,
-    houseID: string,
-    lastName: string,
-    firstName: string,
-    role: string,
-    password: string
-  ) => {},
-  signIn: async (email: string, password: string) => {},
-  register: async (email: string, houseID:string, first_name: string, last_name: string, role: string, password: string) => {},
-  signOutAuth: () => {},
-	deleteUser: async (uid: string) => {}
+	authUser: defaultUser, setAuthUser: ()=>{},
+//   authUser: defaultUser,
+//   setAuthUser: (user: User) => {}
+//   house: defaultHouse,
+//   setAuthUser: () => {},
+//   register: async (
+//     email: string,
+//     houseID: string,
+//     lastName: string,
+//     firstName: string,
+//     role: string,
+//     password: string
+//   ) => {},
+//   signIn: async (email: string, password: string) => {},
+//   signOutAuth: () => {},
+// 	deleteUser: async (uid: string) => {}
 });
 
 export const AuthUserProvider = ({children}: any) => {
+	const {authUser, setAuthUser} = useContext(authUserContext)
+	
+	const [user, setUser] = useState(defaultUser);
 	const auth = useFirebaseAuth();
+	setUser(auth.authUser);
+
+	const value = {authUser:user, setAuthUser:setUser};
+
 	return (
-		<authUserContext.Provider value = {auth}> {children} </authUserContext.Provider>
+		<authUserContext.Provider value = {value}> {children} </authUserContext.Provider>
 	)
 }
 
