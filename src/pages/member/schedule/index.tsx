88416a109f2a3e4e6ccbde9289f6064ee9a10d8b
Danashi Messele <danashimessele@berkeley.edu>
2022-11-29 16:12:38 -0800

    testing

--- src/pages/member/schedule/index.tsx
+++ src/pages/member/schedule/index.tsx
@@ -2,6 +2,7 @@ import { Typography } from "@mui/material";
 import React, { useEffect, useState } from "react";
 import Layout from "../../../components/Layout/Layout";
 import { MemberScheduleHeader } from "../../../components/MemberComponents/MemberSchedule/MemberScheduleHeader/MemberScheduleHeader";
+import { MemberShiftFilters } from "../../../components/MemberComponents/MemberSchedule/MemberShiftFilters/MemberShiftFilters";
 import { MemberShiftSchedule } from "../../../components/MemberComponents/MemberSchedule/MemberShiftSchedule/MemberShiftSchedule";
 import { useUserContext } from "../../../context/UserContext";
 import  {useFirebaseAuth}  from "../../../firebase/queries/auth";
@@ -10,26 +11,26 @@ import { House, User } from "../../../types/schema";
 import styles from "./MemberSchedule.module.css";
 
 type MemberSchedulePageProps = {
-	user: User,
-	house: House,
+	user?: User,
+	house?: House,
 }
 
 const MemberSchedulePage: React.FC<MemberSchedulePageProps> = ({
 	user,
 	house
 }) => {
-	const { authUser, signIn, signOutAuth } = useUserContext();
+	const { authUser, signIn, signOutAuth, register } = useUserContext();
 	console.log("AUTHUSER", authUser)
 
 	return (
 		<Layout>
 			<MemberScheduleHeader/>
-			<button onClick={() => signIn("test123@gmail.com", "test123")}> sign in</button>
-			<h1>{authUser.first_name}</h1>
-			<button onClick={() => signOutAuth()}> sign out</button>
-
-			<MemberShiftSchedule member=""/>
-
+			<div className={styles.center}>
+				<MemberShiftFilters />
+			</div>
+			<div className={styles.table}>
+				<MemberShiftSchedule user = {authUser}/>
+			</div>
 		</Layout>
 	)
 }
