81874734d1ceed696e4707b532f7187d501a826c
ken-mar <kenmartinez@berkeley.edu>
2022-12-02 17:36:05 -0800

    i dunno why

--- src/pages/manager/schedule/index.tsx
+++ src/pages/manager/schedule/index.tsx
@@ -1,11 +1,12 @@
 import Layout from "../../../components/Layout/Layout";
 import ShiftSchedule from "../../../components/ManagerComponents/shiftSchedule/ShiftSchedule";
+import UnassignedShiftList from "../../../components/ManagerComponents/UnassignedShiftList/unassignedShiftList";
 
 export default function SchedulePage() {
   return (
     <Layout>
       <p>This is the schedule page.</p>
-      <ShiftSchedule />
+      <UnassignedShiftList />
     </Layout>
   );
 }
