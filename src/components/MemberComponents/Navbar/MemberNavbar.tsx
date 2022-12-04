313f815040a6c0a3c73089e0eb60dc79c728a3c6
9samioh <63632947+9samioh@users.noreply.github.com>
2022-12-04 12:44:41 -0800

    updated member settings page (#39)
    * updated member settings page
    
    * fixed the firebase error by changing the order of stuff in logout button
    
    Co-authored-by: Gregoria  Millensifer <gregoriiaaa@berkeley.edu>

--- src/components/MemberComponents/Navbar/MemberNavbar.tsx
+++ src/components/MemberComponents/Navbar/MemberNavbar.tsx
@@ -119,19 +119,15 @@ const MemberNavbar: React.FunctionComponent = () => {
   );
 
   const logout = () => (
-    /**
-     * Renders the logout button
-     *
-     * onClick handler pushes "/login" to the url using router and signs out the current user in context
-     */
-    <List className={styles.logout}>
+    <div className={styles.logout}>
       <ListItem
         className={styles.item}
         button
         key={"settings"}
         onClick={() => {
-          router.push("/login");
+          // signout MUST happen before pushing the login page, or else there is an error cuz the user context tries to use an empty user
           signOutAuth();
+          router.push("/login");
         }}
       >
         <Icon type="navLogout" />
@@ -145,7 +141,7 @@ const MemberNavbar: React.FunctionComponent = () => {
           primary={"Logout"}
         />
       </ListItem>
-    </List>
+    </div>
   );
 
   return (
