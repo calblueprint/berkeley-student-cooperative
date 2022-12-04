2f06e2206a0539d3bdb758c9d6eaec7c47d819c9
Andrei Tan <73627326+andreisito@users.noreply.github.com>
2022-12-03 22:36:06 -0800

    Andrei/member view shift card (#19)
    * basic functionality
    
    * Working button
    
    * Char limit added to Verification
    
    * checkpoint
    
    * Naively working
    
    * Testing finished
    
    * Documentation
    
    * resolved merge :D
    
    * Verification button disabled if already verified
    
    * notes updated
    
    * cleaned up root index, and added view shift modal in pages/member/schedule index
    
    * testing and deleted unused imports
    
    Co-authored-by: Andrei Tan <ManTan.875@berkeley.edu>
    Co-authored-by: Gregoria  Millensifer <gregoriiaaa@berkeley.edu>

--- src/pages/member/schedule/index.tsx
+++ src/pages/member/schedule/index.tsx
@@ -1,9 +1,11 @@
 import Layout from "../../../components/Layout/Layout";
+import ViewShiftcard from "../../../components/MemberComponents/ViewShiftcard/ViewShiftcard";
 
 export default function SchedulePage() {
-    return (
-        <Layout>
-            <p>This is the schedule page.</p>
-        </Layout>
-    )
-  }
+  return (
+    <Layout>
+      <p>This is the schedule page.</p>
+      <ViewShiftcard shiftID={"gTyVkT3TAh6NX0oHangP"} houseID={"EUC"} />
+    </Layout>
+  );
+}
