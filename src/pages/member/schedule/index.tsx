c944c383cccfdebc45f8936e1ed3867641bff5b9
Gregoria  Millensifer <gregoriiaaa@berkeley.edu>
2022-12-03 17:44:02 -0800

    cleaned up root index, and added view shift modal in pages/member/schedule index

--- src/pages/member/schedule/index.tsx
+++ src/pages/member/schedule/index.tsx
@@ -1,9 +1,11 @@
 import Layout from "../../../components/Layout/Layout";
+import ViewShiftcard from "../../../components/MemberComponents/ViewShiftcard/ViewShiftcard";
 
 export default function SchedulePage() {
-    return (
-        <Layout>
-            <p>This is the schedule page.</p>
-        </Layout>
-    )
-  }
+  return (
+    <Layout>
+      <p>This is the schedule page.</p>
+      <ViewShiftcard shiftID={"iBVA4gOntEGFA4AxpqFU"} houseID={"EUC"} />
+    </Layout>
+  );
+}
