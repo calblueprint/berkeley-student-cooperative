e987753c911bd644ad9f8eb6a287e87c90617f99
Gregoria  Millensifer <gregoriiaaa@berkeley.edu>
2023-02-22 16:19:28 -0800

    Revert "fixed almost all eslint errors on files i worked on (#50)"
    This reverts commit 7612f2a63e9a510b490ae902283f9a3d7470ea2e.
    
    reverting back because other files actually use houseQueries.tsx

--- src/pages/member/schedule/index.tsx
+++ src/pages/member/schedule/index.tsx
@@ -4,20 +4,18 @@ import { MemberScheduleHeader } from '../../../components/MemberComponents/Membe
 import { MemberShiftView } from '../../../components/MemberComponents/MemberSchedule/MemberShiftView/MemberShiftView'
 import { useUserContext } from '../../../context/UserContext'
 import { House, User } from '../../../types/schema'
-// import styles from './MemberSchedule.module.css'
+import styles from './MemberSchedule.module.css'
 
 type MemberSchedulePageProps = {
   user?: User
   house?: House
 }
 
-const MemberSchedulePage: React.FC<MemberSchedulePageProps> = (
-  {
-    // user,
-    // house,
-  }
-) => {
-  const { authUser } = useUserContext()
+const MemberSchedulePage: React.FC<MemberSchedulePageProps> = ({
+  user,
+  house,
+}) => {
+  const { authUser, signIn, signOutAuth, register } = useUserContext()
 
   return (
     <Layout>
