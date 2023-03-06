d94088510814f77bdd9161004e4b91771486f391
Christian Castaneda <christianjc_09@yahoo.com>
2023-03-06 03:52:58 -0800

    added packages and added CustomFormikFields file

---
+++ src/components/ManagerComponents/Shiftcard/ShiftCardTest.tsx
@@ -0,0 +1,56 @@
+import ShiftForm from './ShiftForm'
+import {
+  Button,
+  Dialog,
+  Typography,
+  DialogContent,
+  DialogTitle,
+} from '@mui/material'
+import { useState } from 'react'
+
+const initialValues = {
+  name: '',
+  category: '',
+  credit_hours: '',
+  day: '',
+  description: '',
+}
+
+function ShiftCardTest() {
+  const [open, setOpen] = useState(false)
+  // const [shiftValues, setShiftValues] = useState(null)
+  const handleClose = () => {
+    setOpen(false)
+  }
+
+  const handleOpen = () => {
+    setOpen(true)
+  }
+  return (
+    <>
+      <Button fullWidth variant="outlined" onClick={handleOpen}>
+        <Typography>Add Shift</Typography>
+      </Button>
+
+      <Dialog
+        fullWidth
+        maxWidth="md"
+        open={open}
+        onClose={handleClose}
+        className="dialog"
+      >
+        <DialogTitle variant="h4" component="h2">
+          Create Shift
+        </DialogTitle>
+        <DialogContent>
+          <ShiftForm
+            setOpen={setOpen}
+            // shiftId='6401c47de8d154aa9ccf5d93'
+            isNewShift={true}
+          />
+        </DialogContent>
+      </Dialog>
+    </>
+  )
+}
+export default ShiftCardTest
