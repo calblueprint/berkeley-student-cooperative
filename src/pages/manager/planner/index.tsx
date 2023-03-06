4bedefa4955d12dadc70bf5d2bcc3f41e940610c
Christian Castaneda <christianjc_09@yahoo.com>
2023-03-06 01:55:39 -0800

    setting up testing component for redux

--- src/pages/manager/planner/index.tsx
+++ src/pages/manager/planner/index.tsx
@@ -5,7 +5,7 @@ import CategoriesView from '../categoryDropdown/categoriesView'
 import { useUserContext } from '../../../context/UserContext'
 import ShiftSchedule from '../../../components/ManagerComponents/shiftSchedule/ShiftSchedule'
 import { UnassignedTabContent } from './UnassignedTabContent'
-import Testing from './Testing'
+import ReduxTesting from './ReduxTesting'
 
 interface TabPanelProps {
   children?: React.ReactNode
@@ -63,7 +63,7 @@ export default function SchedulePage() {
         </Box>
         <TabPanel value={currPage} index={0}>
           <UnassignedTabContent />
-          <Testing />
+          <ReduxTesting />
         </TabPanel>
         <TabPanel value={currPage} index={1}>
           <ShiftSchedule />
