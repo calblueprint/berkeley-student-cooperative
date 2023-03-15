b3da06e61c4117432feb89d885bc100ef5053b0c
Gregoria  Millensifer <gregoriiaaa@berkeley.edu>
2023-03-15 03:17:38 -0700

    deleted files not ready for deployment or not connected to the manager planner flow

--- src/pages/manager/schedule/index.tsx
+++ src/pages/manager/schedule/index.tsx
@@ -1,6 +1,5 @@
 import { Box, Typography } from '@mui/material'
 import Layout from '../../../components/Layout/Layout'
-// import ShiftSchedule from '../../../components/ManagerComponents/shiftSchedule/ShiftSchedule'
 
 export default function SchedulePage() {
   return (
@@ -11,7 +10,6 @@ export default function SchedulePage() {
       <Box sx={{ width: '100%' }}>
         <Typography color={'#000'}>Coming Soon!!!</Typography>
       </Box>
-      {/* <ShiftSchedule /> */}
     </Layout>
   )
 }
--- src/pages/member/house/index.tsx
+++ src/pages/member/house/index.tsx
@@ -1,9 +1,15 @@
+import { Box, Typography } from '@mui/material'
 import Layout from '../../../components/Layout/Layout'
 
 export default function HousePage() {
   return (
     <Layout>
-      <p>This is the house page.</p>
+      <Typography variant="h4" color={'#000'}>
+        House Schedule
+      </Typography>
+      <Box sx={{ width: '100%' }}>
+        <Typography color={'#000'}>Coming Soon!!!</Typography>
+      </Box>
     </Layout>
   )
 }
