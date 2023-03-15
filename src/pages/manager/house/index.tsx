b41ff950f527dc0f7df853189aa5e588c2d3eca0
Gregoria  Millensifer <gregoriiaaa@berkeley.edu>
2023-03-15 01:48:40 -0700

    deleting files that are not ready for deployment

--- src/pages/manager/house/index.tsx
+++ src/pages/manager/house/index.tsx
@@ -1,7 +1,5 @@
 import { Box, Typography } from '@mui/material'
-// import Head from 'next/head'
 import Layout from '../../../components/Layout/Layout'
-// import ParseCSV from '../../ParseCsv/ParseCsv'
 
 export default function SchedulePage() {
   return (
@@ -12,21 +10,6 @@ export default function SchedulePage() {
       <Box sx={{ width: '100%' }}>
         <Typography color={'#000'}>Coming Soon!!!</Typography>
       </Box>
-      {/* <div>
-        <Head>
-          <title>Workshift App</title>
-          <meta name="description" content="Next.js firebase Workshift app" />
-          <link rel="icon" href="/favicon.ico" />
-        </Head>
-        <main>
-          <ParseCSV />
-        </main>
-        <footer>
-          <a href="#" rel="noopener noreferrer">
-            Workshift App
-          </a>
-        </footer>
-      </div> */}
     </Layout>
   )
 }
